f6a53071a65cc3d6bbaeb2f7460cdce839abbfa5
Utsab Bera <nCoder24@users.noreply.github.com>
2023-06-10 00:50:05 +0530

    add border to canvas

--- main.js
+++ main.js
@@ -6,11 +6,13 @@ const hideCursor = () => process.stdout.write('\u001B[?25l');
 
 const main = () => {
   const frame = new Frame(0, 0, 10, 10, process.stdout);
-  const canvas = new Canvas(3, 3);
+  const canvas = new Canvas(5, 3);
   hideCursor();
   console.clear();
   frame.add(canvas, 2, 2);
-  canvas.put("--", 0, 0);
+  canvas.put("--", 1, 1);
+  canvas.put("@@", 0, 1);
+  canvas.drawBorder();
   
   process.stdin.read();
 }
--- src/canvas.js
+++ src/canvas.js
@@ -1,24 +1,59 @@
 const EventEmitter = require("events");
+const blankPixel = "  ";
 
 class Canvas extends EventEmitter {
   #pixels;
+  #borderWidth;
 
   constructor(hight, width) {
     super();
-    this.#pixels = new Array(hight).fill().map(() => new Array(width).fill("**"));
+    this.#pixels = new Array(hight)
+      .fill()
+      .map(() => new Array(width).fill(blankPixel));
+    this.#borderWidth = 0;
   }
 
   put(pixel, x, y) {
-    this.#pixels[x][y] = pixel;
-    this.emit("put", pixel, x, y);
+    const absX = x + this.#borderWidth;
+    const absY = y + this.#borderWidth;
+
+    this.#pixels[absX][absY] = pixel;
+    this.emit("put", pixel, absX, absY);
   }
 
   sync() {
     this.#pixels.forEach((col, x) => {
       col.forEach((pixel, y) => {
-        this.put(pixel, x, y);
+        this.emit("put", pixel, x, y);
       });
     });
+  }
+
+  drawBorder() {
+    this.#borderWidth = 1;
+    const { thinBorder } = require("../resourses/box-chars.json");
+    this.#pixels.forEach((col) => {
+      const horizontalLine = thinBorder.horizontalLine.repeat(2);
+      col.unshift(horizontalLine);
+      col.push(horizontalLine);
+    });
+
+    const leftCol = (
+      thinBorder.topLeftCorner +
+      thinBorder.leftVirticleLine.repeat(this.#pixels[0].length - 2) +
+      thinBorder.bottomLeftCorner
+    ).match(/../g);
+
+    const rightCol = (
+      thinBorder.topRightCorner +
+      thinBorder.rightVirticleLine.repeat(this.#pixels[0].length - 2) +
+      thinBorder.bottomRightCorner
+    ).match(/../g);
+
+    this.#pixels.unshift(leftCol);
+    this.#pixels.push(rightCol);
+
+    this.sync();
   }
 }
 
